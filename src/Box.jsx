305d6d90a993a371fc8468b6207001180fc08324
Soham Ghosh <ghoshsoham1708@gmail.com>
2025-03-03 00:18:55 +0530

    2.0

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,21 +1,24 @@
 import './boxstyle.css'
 import Showsmallbox from './Smallbox'
-
+import { useState } from 'react';
 const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 
-const colors=['Red','Blue','Green','Yellow','Pink','Purple','Cyan','Magenda']
+const colors=['Red','Blue','Green','Yellow','Pink','Purple','Cyan','Magenda','Orange','Violet']
 // const randomcolor=()=>{"#" + Math.floor(Math.random() * 16777215).toString(16)}
-
-const randcolor=colors[Math.floor(Math.random() * colors.length)]
-
+const [colour,setColour] = useState("red")
+const randcolor=()=>colors[Math.floor(Math.random() * colors.length)]
+const changecolour=()=>{
+    setColour(randcolor())
+}
 export default function Box(){
     return(
         <div className="bigbox">
             {
                 numbers.map((num) => (
-                    <Showsmallbox number={num} bgColor={randcolor}/>
+                    <Showsmallbox onClick = {changecolour} number={num} bgColor={randcolor()}/>
                     )
                 )
+               
             }
         </div>
     )
