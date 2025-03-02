096dd62de3e86b6084db5aced4136b613d632510
Soham Ghosh <ghoshsoham1708@gmail.com>
2025-03-03 01:27:07 +0530

    Finalized

--- src/Box.jsx
+++ src/Box.jsx
@@ -5,7 +5,7 @@ const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 
 const colors=["red", "blue", "green", "yellow", "orange", 
   "purple", "pink", "brown", "gray", 
-  "white", "cyan", "magenta", "lime", "teal", 
+   "cyan", "magenta", "lime", "teal", 
   "indigo", "violet", "gold", "silver", "beige", 
   "maroon", "navy", "olive", "coral", "turquoise"]
 const randcolor=()=>colors[Math.floor(Math.random() * colors.length)]
